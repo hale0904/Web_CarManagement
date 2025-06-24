69e7694ae905da247f33d3d1d3c46f1988fe1313
hale0904 <leduyha2809@gmail.com>
2025-06-24 18:56:34 +0700

    chore(project): setup initial project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './views/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: Home, title: 'Car'
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './views/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
