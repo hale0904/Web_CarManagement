9255ba5a805599b391c74d96009b9cc31203a9ad
hale0904 <leduyha2809@gmail.com>
2025-06-28 12:33:44 +0700

    feat(admin/home): add new content to admin home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 
 import { Home } from '@views/home/home.component';
 import { Login } from './authentication/login/login.component';
-
 import { AuthGuard } from '@core/guard/auth.guard';
-import { Layout } from './shared/layout/layout.component';
 import { QuanLySanPhamAdmin } from './views/admin/quanlysanpham/quanlysanpham.component';
 import { QuanLyKhachHangAdmin } from './views/admin/quanlykhachhang/quanlykhachhang.component';
 import { DeviceGuard } from '@core/guard/device.guard';
+import { HomeAdmin } from './views/admin/home-admin/home-admin.component';
+import { Layout } from './shared/layout/layout.component';
 
 export const routes: Routes = [
     { path: '', component: Home, title: 'Car' },
@@ -20,6 +20,7 @@ export const routes: Routes = [
         canActivate: [AuthGuard, DeviceGuard], 
         title: 'Trang chủ Admin',
         children: [
+            { path: '', component: HomeAdmin, title: 'Trang chủ admin'},
             { path: 'products', component: QuanLySanPhamAdmin, title: 'Quản lý sản phẩm'},
             { path: 'customers', component: QuanLyKhachHangAdmin, title: 'Quản lý khách hàng'}
         ]
--- src/app/views/admin/home-admin/home-admin.component.ts
+++ src/app/views/admin/home-admin/home-admin.component.ts
@@ -1,10 +1,8 @@
 import { Component } from "@angular/core";
-import { Layout } from "@shared/layout/layout.component";
 
 @Component ({
     selector: 'app-home-admin',
     standalone: true,
-    imports: [Layout],
     templateUrl: './home-admin.component.html',
     styleUrl: './home-admin.component.css'
 })
